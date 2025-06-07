1e3a8df8e98dcca5e18f7a8ed1bfb4ec0078ab8b
SuperKing <73359432+devsuperking@users.noreply.github.com>
2025-06-07 13:33:02 +0200

    REMOVE SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
+#include <SDL3/SDL.h>
+#include <stdio.h>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL3 FPS Example", 800, 600, SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        SDL_Log("CreateWindow Error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window);
+    if (!renderer) {
+        SDL_Log("CreateRenderer Error: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(1);  // Yield to OS
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
