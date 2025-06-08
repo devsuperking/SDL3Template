ee05ad9a5b3984d49a2e832b9cf0ed904ac08918
SuperKing <73359432+devsuperking@users.noreply.github.com>
2025-06-08 16:47:57 +0200

    main.cpp template

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,51 @@
 #include <SDL3/SDL.h>
-#include <SDL3_image/SDL_image.h>
-#include <SDL3_ttf/SDL_ttf.h>
-#include <iostream>
 
-int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL Init failed: " << SDL_GetError() << std::endl;
+int main(int argc, char *argv[]) {
+
+    // Initialize SDL
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError());
         return 1;
     }
 
+    // Create a window
+    SDL_Window *window = SDL_CreateWindow("SDL3 Background Color", 800, 600, SDL_WINDOW_OPENGL);
+    if (!window) {
+        SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create a renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        SDL_Log("SDL_CreateRenderer Error: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderVSync(renderer, -1);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    SDL_Event e;
+    bool quit = false;
+
+    // Game loop
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) quit = true;
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1000 / 165);
+    }
+
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
